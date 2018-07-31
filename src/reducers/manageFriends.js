0325d18924862a2774b90d5ee75fc24b0532a1d8
Kelsey Fecho <kfecho30@gmail.com>
2018-07-31 14:53:01 +0000

    automatically backed up by learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,7 +1,7 @@
 export function manageFriends(state, action){
   switch(action.type){
     case 'ADD_FRIEND':
-      return {friends: Object.assign([...state.friends, action.friend])}
+      return {friends: [...state.friends, action.friend]}
     case 'REMOVE_FRIEND':
       return
     default:
