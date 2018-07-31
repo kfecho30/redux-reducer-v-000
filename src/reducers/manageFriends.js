1a8f7b9dc4bfa3cd91ebb16c497727905a4edd3a
Kelsey Fecho <kfecho30@gmail.com>
2018-07-31 14:55:21 +0000

    automatically backed up by learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -3,7 +3,7 @@ export function manageFriends(state, action){
     case 'ADD_FRIEND':
       return {friends: [...state.friends, action.friend]}
     case 'REMOVE_FRIEND':
-      return
+      return {friends: [...state.friends.slice(0, out), ...state.friends.slice(out + 1)]}
     default:
       return state;
   }
