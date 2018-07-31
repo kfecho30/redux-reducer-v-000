c45ce801dd37de95ce8163a0c2b6d00c1e43b29f
Kelsey Fecho <kfecho30@gmail.com>
2018-07-31 14:55:53 +0000

    automatically backed up by learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -3,6 +3,7 @@ export function manageFriends(state, action){
     case 'ADD_FRIEND':
       return {friends: [...state.friends, action.friend]}
     case 'REMOVE_FRIEND':
+      const out = state.friends.findIndex(friend => friend.id == action.id)
       return {friends: [...state.friends.slice(0, out), ...state.friends.slice(out + 1)]}
     default:
       return state;
